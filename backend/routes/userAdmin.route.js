8d96860c3bef248abb305395a08584332f2d092f
Fuadekoo <fuadabdurahman0859@gmail.com>
2024-06-04 02:23:54 +0400

    admin checher is used

--- backend/routes/userAdmin.route.js
+++ backend/routes/userAdmin.route.js
@@ -1,12 +1,13 @@
 import express from 'express';
 
 import {addNewUser,salectAllUser,updateAdmin} from '../controllers/userAdmin.controller.js';//  Assuming you have a add new user controller defined
+import roleCheckMiddleware from '../middlewares/adminCheckerMiddleware.js';
 const router=express.Router();
 
 
 
 // Register new Adminuser
-router.post("/signup",adminCheckerMiddleware, addNewUser);
+router.post("/signup",roleCheckMiddleware, addNewUser);
 router.get("/allusers", salectAllUser);
 router.put("/update-admin/:id",updateAdmin);
 
