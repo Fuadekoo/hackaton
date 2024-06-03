fdc63bf7bb164a7652b5de6e0f5f7fbf6fad15c9
Fuadekoo <fuadabdurahman0859@gmail.com>
2024-06-03 12:42:22 +0400

    frist commmit

--- backend/server.js
+++ backend/server.js
@@ -20,7 +20,7 @@ app.use(cookieParser());
 
 
 // listen to server
-const PORT = process.env.PORT || 6000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
